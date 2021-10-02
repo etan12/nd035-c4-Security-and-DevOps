318c77618d02b1f766d1528ca107002635bee6a6
Edwin Tan <etan12@umd.edu>
2021-10-02 16:52:59 -0600

    add example unit tests

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,21 @@
 package com.udacity.examples.Testing;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class HelperTest {
 
+    @Test
+    public void getCount() {
+        List<String> empNames = Arrays.asList("sareeta", "udacity");
+        final long actual = Helper.getCount(empNames);
+        assertEquals(2, actual);
+    }
 	
 }
